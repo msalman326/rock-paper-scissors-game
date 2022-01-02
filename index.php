7d0bb5b6636d28109cf478deded40fdee0bc6971
msalman326 <salmanrazzaq326@gmail.com>
2022-01-02 20:40:11 +0500

    rock paper sc done

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MUHAMMAD SALMAN RAZZAQ - Broken Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to  Rock Paper Scissors</h1>
+<p><strong> To play game click on login. And then play. Password is php123 .. You can set any username.</strong>
+</p>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+
+</div>
+</body>
+
